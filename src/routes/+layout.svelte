538edf4c6ec4f785c801a3647f8a6d97b66f316f
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-01-04 17:50:15 +0100

    feat: add layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,24 @@
 <script>
-	import '../app.css';
+	import '../app.css'
+
+	import { page } from '$app/stores'
 </script>
 
+<svelte:head>
+	<title>
+		{$page.data.title
+			? //
+			  `${$page.data.title} – Install PWA`
+			: //
+			  'Install PWA'}
+	</title>
 
-<slot />
+	<meta
+		name="description"
+		content={$page.data.description}
+	/>
+</svelte:head>
+
+<div class="container mx-auto px-4 sm:px-6 lg:px-8">
+	<slot />
+</div>
