862a56f35718ff78bd3bd02c590dd9e3f3ee6352
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-01-04 17:49:13 +0100

    feat: use inter font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,15 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+			}
+		}
 	},
 
 	plugins: [require('daisyui')]
