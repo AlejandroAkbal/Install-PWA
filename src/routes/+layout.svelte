b250e8e468d505356c46e3f054473abc2eb47d81
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-01-05 12:08:19 +0100

    feat: add description and og image

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,13 +10,21 @@
 			? //
 			  `${$page.data.title} – Install PWA`
 			: //
-			  'Install PWA'}
+			  'Install PWA – Easy App installation instructions'}
 	</title>
 
 	<meta
 		name="description"
-		content={$page.data.description}
+		content={$page.data.description ?? 'Easily explain how to install your PWA to your end users. For free!'}
 	/>
+
+	<!-- OG -->
+	{#if $page.data.imageUrl}
+		<meta
+			property="og:image"
+			content={$page.data.imageUrl}
+		/>
+	{/if}
 </svelte:head>
 
 <div class="container mx-auto px-4 sm:px-6 lg:px-8">
--- src/routes/from/[domain]/+page.js
+++ src/routes/from/[domain]/+page.js
@@ -1,15 +1,17 @@
-import { error } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit'
 
 /** @type {import('./$types').PageLoad} */
 export function load({ params }) {
 	if (!params.domain) {
-		throw error(404, 'Not found');
+		throw error(404, 'Not found')
 	}
 
 	return {
 		title: params.domain,
 		description: `Learn how to install the ${params.domain} app!`,
 
+		imageUrl: `https://url-shot.api-point.cf/?width=1200&height=630&url=${params.domain}`,
+
 		domain: params.domain
-	};
+	}
 }
