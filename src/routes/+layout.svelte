0b35c4e3cf18641da0064445d72325530b21a81d
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-01-08 14:18:19 +0100

    feat: improve header and app layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script>
 	import '../app.css'
 
-	import { page } from '$app/stores'
+	import { page, navigating } from '$app/stores'
+	import { slide } from 'svelte/transition'
+
+	let mobileMenuOpen = false
+
+	$: if ($navigating) {
+		mobileMenuOpen = false
+	}
 </script>
 
 <svelte:head>
@@ -27,6 +34,120 @@
 	{/if}
 </svelte:head>
 
-<div class="container mx-auto px-4 sm:px-6 lg:px-8">
-	<slot />
-</div>
+<header class="relative">
+	<!-- Desktop menu -->
+	<div class="bg-base-200 py-6">
+		<nav
+			class="relative mx-auto flex max-w-7xl items-center justify-between px-6"
+			aria-label="Global"
+		>
+			<div class="flex flex-1 items-center">
+				<div class="flex w-full items-center justify-between md:w-auto">
+					<a href="/">
+						<span class="sr-only">Install PWA</span>
+						<img
+							class="h-8 w-auto sm:h-10"
+							src="/favicon.svg"
+							alt="Logo"
+						/>
+					</a>
+					<div class="-mr-2 flex items-center md:hidden">
+						<button
+							on:click={() => (mobileMenuOpen = !mobileMenuOpen)}
+							type="button"
+							class="focus-ring-inset inline-flex items-center justify-center rounded-md bg-base-200 p-2 text-base-content hover:bg-base-300 focus:outline-none focus:ring-2 focus:ring-primary-focus"
+							aria-expanded="false"
+						>
+							<span class="sr-only">Open main menu</span>
+							<!-- Heroicon name: outline/bars-3 -->
+							<svg
+								class="h-6 w-6"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								aria-hidden="true"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+								/>
+							</svg>
+						</button>
+					</div>
+				</div>
+				<div class="hidden space-x-8 md:ml-10 md:flex">
+					<a
+						href="/from/www.adidas.com"
+						class="text-base font-medium link link-hover">Example</a
+					>
+				</div>
+			</div>
+		</nav>
+	</div>
+
+	<!--
+          Mobile menu, show/hide based on menu open state.
+  
+          Entering: "duration-150 ease-out"
+            From: "opacity-0 scale-95"
+            To: "opacity-100 scale-100"
+          Leaving: "duration-100 ease-in"
+            From: "opacity-100 scale-100"
+            To: "opacity-0 scale-95"
+        -->
+	{#if mobileMenuOpen}
+		<div
+			transition:slide={{ duration: 200 }}
+			class="absolute inset-x-0 top-0 origin-top transform p-2 transition md:hidden"
+		>
+			<div class="overflow-hidden rounded-lg bg-base-100 shadow-md ring-1 ring-base-content ring-opacity-5">
+				<div class="flex items-center justify-between px-5 pt-4">
+					<p>👋</p>
+
+					<div class="-mr-2">
+						<button
+							on:click={() => (mobileMenuOpen = !mobileMenuOpen)}
+							type="button"
+							class="inline-flex items-center justify-center rounded-md bg-base-100 p-2 text-base-content hover:bg-base-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-primary-focus"
+						>
+							<span class="sr-only">Close menu</span>
+							<!-- Heroicon name: outline/x-mark -->
+							<svg
+								class="h-6 w-6"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								aria-hidden="true"
+							>
+								<path
+									stroke-linecap="round"
+									stroke-linejoin="round"
+									d="M6 18L18 6M6 6l12 12"
+								/>
+							</svg>
+						</button>
+					</div>
+				</div>
+
+				<div class="pt-5 pb-6">
+					<!-- Links -->
+					<div class="space-y-1 px-2">
+						<a
+							href="/from/www.adidas.com"
+							class="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-50"
+						>
+							Example
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	{/if}
+</header>
+
+<slot />
--- src/routes/from/[domain]/+page.svelte
+++ src/routes/from/[domain]/+page.svelte
@@ -36,184 +36,186 @@
 	})
 </script>
 
-<main class="py-6">
-	<section>
-		<div class="grid place-items-center gap-3">
-			<img
-				src="https://www.google.com/s2/favicons?domain={data.domain}"
-				alt="Favicon of '{data.domain}' domain"
-				style="width: 48px; height: 48px;"
-			/>
+<div class="container mx-auto px-4 sm:px-6 lg:px-8">
+	<main class="py-6">
+		<section>
+			<div class="grid place-items-center gap-3">
+				<img
+					src="https://www.google.com/s2/favicons?domain={data.domain}"
+					alt="Favicon of '{data.domain}' domain"
+					style="width: 48px; height: 48px;"
+				/>
 
-			<!-- Truncate text -->
-			<h1 class="text-2xl truncate font-semibold">
-				{data.domain}
-			</h1>
+				<!-- Truncate text -->
+				<h1 class="text-2xl truncate font-semibold">
+					{data.domain}
+				</h1>
 
-			<h2 class="text-lg">
-				Learn how to install the
-				<output>{data.domain}</output> PWA on your device in 4 simple steps
-			</h2>
+				<h2 class="text-lg">
+					Learn how to install the
+					<output>{data.domain}</output> PWA on your device in 4 simple steps
+				</h2>
+			</div>
+		</section>
+
+		<div class="mt-6">
+			<TabGroup
+				defaultIndex={selectedTab}
+				on:change={(e) => (selectedTab = e.detail)}
+				class="grid place-items-center gap-6"
+			>
+				<TabList class="max-w-fit tabs tabs-boxed">
+					<Tab class={({ selected }) => (selected ? 'tab tab-active' : 'tab')}>Android</Tab>
+					<Tab class={({ selected }) => (selected ? 'tab tab-active' : 'tab')}>iOS / iPadOS</Tab>
+					<Tab class={({ selected }) => (selected ? 'tab tab-active' : 'tab')}>Desktop</Tab>
+				</TabList>
+
+				<TabPanels>
+					<!-- Android -->
+					<TabPanel>
+						<div class="relative shadow-xl min-h-[525px] overflow-hidden rounded-md border-0">
+							<!-- Image cutout overlay -->
+							<img
+								class="absolute top-0 left-0 z-10"
+								src="/media/img/android-360x780.png"
+								alt="Android installation cutout"
+							/>
+
+							<!-- Domain capture -->
+							<img
+								src="https://url-shot.api-point.cf?width=360&height=780&url={url.toString()}"
+								alt="Capture of '{data.domain}' domain"
+								style="filter: brightness(0.8) contrast(1.2);"
+								class="pt-[82px] object-cover"
+							/>
+						</div>
+					</TabPanel>
+
+					<!-- iOS -->
+					<TabPanel>
+						<div class="relative shadow-xl max-h-[500px] overflow-hidden rounded-md border-0">
+							<!-- Image cutout overlay -->
+							<img
+								class="absolute top-0 left-0 z-10"
+								src="/media/img/ios-1024x715.png"
+								alt="iOS installation cutout"
+							/>
+
+							<!-- Domain capture -->
+							<img
+								src="https://url-shot.api-point.cf?width=1024&height=715&url={url.toString()}"
+								alt="Capture of '{data.domain}' domain"
+								style="filter: brightness(0.7) contrast(1.2);"
+								class="pt-[64px] object-cover"
+							/>
+						</div>
+					</TabPanel>
+
+					<!-- Desktop -->
+					<TabPanel>
+						<div class="relative shadow-xl max-h-[40vh] overflow-hidden rounded-md border-0">
+							<!-- Image cutout overlay -->
+							<img
+								class="absolute top-0 left-0 z-10"
+								src="/media/img/desktop-1920x1080.png"
+								alt="Desktop installation cutout"
+							/>
+
+							<!-- Domain capture -->
+							<img
+								src="https://url-shot.api-point.cf?width=1920&height=1080&url={url.toString()}"
+								alt="Capture of '{data.domain}' domain"
+								style="filter: brightness(0.7) contrast(1.2);"
+								class="pt-[52px] object-cover"
+							/>
+						</div>
+					</TabPanel>
+				</TabPanels>
+			</TabGroup>
 		</div>
-	</section>
 
-	<div class="mt-6">
-		<TabGroup
-			defaultIndex={selectedTab}
-			on:change={(e) => (selectedTab = e.detail)}
-			class="grid place-items-center gap-6"
-		>
-			<TabList class="max-w-fit tabs tabs-boxed">
-				<Tab class={({ selected }) => (selected ? 'tab tab-active' : 'tab')}>Android</Tab>
-				<Tab class={({ selected }) => (selected ? 'tab tab-active' : 'tab')}>iOS / iPadOS</Tab>
-				<Tab class={({ selected }) => (selected ? 'tab tab-active' : 'tab')}>Desktop</Tab>
-			</TabList>
+		<section class="mt-6">
+			<ol class="space-y-4">
+				<li>
+					<h3 class="text-lg font-semibold">Step 1</h3>
 
-			<TabPanels>
-				<!-- Android -->
-				<TabPanel>
-					<div class="relative shadow-xl min-h-[525px] overflow-hidden rounded-md border-0">
-						<!-- Image cutout overlay -->
-						<img
-							class="absolute top-0 left-0 z-10"
-							src="/media/img/android-360x780.png"
-							alt="Android installation cutout"
-						/>
+					{#if selectedTab === 0}
+						Connect to a WiFi network and
+					{/if}
 
-						<!-- Domain capture -->
-						<img
-							src="https://url-shot.api-point.cf?width=360&height=780&url={url.toString()}"
-							alt="Capture of '{data.domain}' domain"
-							style="filter: brightness(0.8) contrast(1.2);"
-							class="pt-[82px] object-cover"
-						/>
-					</div>
-				</TabPanel>
-
-				<!-- iOS -->
-				<TabPanel>
-					<div class="relative shadow-xl max-h-[500px] overflow-hidden rounded-md border-0">
-						<!-- Image cutout overlay -->
-						<img
-							class="absolute top-0 left-0 z-10"
-							src="/media/img/ios-1024x715.png"
-							alt="iOS installation cutout"
-						/>
-
-						<!-- Domain capture -->
-						<img
-							src="https://url-shot.api-point.cf?width=1024&height=715&url={url.toString()}"
-							alt="Capture of '{data.domain}' domain"
-							style="filter: brightness(0.7) contrast(1.2);"
-							class="pt-[64px] object-cover"
-						/>
-					</div>
-				</TabPanel>
-
-				<!-- Desktop -->
-				<TabPanel>
-					<div class="relative shadow-xl max-h-[40vh] overflow-hidden rounded-md border-0">
-						<!-- Image cutout overlay -->
-						<img
-							class="absolute top-0 left-0 z-10"
-							src="/media/img/desktop-1920x1080.png"
-							alt="Desktop installation cutout"
-						/>
-
-						<!-- Domain capture -->
-						<img
-							src="https://url-shot.api-point.cf?width=1920&height=1080&url={url.toString()}"
-							alt="Capture of '{data.domain}' domain"
-							style="filter: brightness(0.7) contrast(1.2);"
-							class="pt-[52px] object-cover"
-						/>
-					</div>
-				</TabPanel>
-			</TabPanels>
-		</TabGroup>
-	</div>
-
-	<section class="mt-6">
-		<ol class="space-y-4">
-			<li>
-				<h3 class="text-lg font-semibold">Step 1</h3>
-
-				{#if selectedTab === 0}
-					Connect to a WiFi network and
-				{/if}
-
-				<a
-					href={url.toString()}
-					target="_blank"
-					rel="noopener noreferrer"
-					class="link link-info"
-				>
-					Open the website
-				</a>
-
-				{#if selectedTab === 0}
-					in Google Chrome
-				{/if}
-			</li>
-
-			<li>
-				<h3 class="text-lg font-semibold">Step 2</h3>
-
-				{#if selectedTab === 0}
-					Click the <kbd class="kbd kbd-sm">More</kbd> button
-
-					<!--  -->
-				{:else if selectedTab === 1}
-					Click the <kbd class="kbd kbd-sm">Share</kbd> button
-
-					<!--  -->
-				{:else}
-					-
-				{/if}
-			</li>
-
-			<li>
-				<h3 class="text-lg font-semibold">Step 3</h3>
-
-				{#if selectedTab === 0}
-					Click <kbd class="kbd kbd-sm">Install App</kbd>
-
-					<div
-						class="block text-left tooltip tooltip-bottom tooltip-primary mt-2"
-						data-tip="Try again later! Or just continue, it will still work"
+					<a
+						href={url.toString()}
+						target="_blank"
+						rel="noopener noreferrer"
+						class="link link-info"
 					>
-						<button class="btn btn-ghost btn-xs no-animation pl-0"> Only see "Add to Home Screen"? </button>
-					</div>
+						Open the website
+					</a>
 
-					<!--  -->
-				{:else if selectedTab === 1}
-					Click <kbd class="kbd kbd-sm">Add to Home Screen</kbd>
+					{#if selectedTab === 0}
+						in Google Chrome
+					{/if}
+				</li>
 
-					<!--  -->
-				{:else}
-					Click <kbd class="kbd kbd-sm">Install</kbd>
-				{/if}
-			</li>
+				<li>
+					<h3 class="text-lg font-semibold">Step 2</h3>
 
-			<li>
-				<h3 class="text-lg font-semibold">Step 4</h3>
+					{#if selectedTab === 0}
+						Click the <kbd class="kbd kbd-sm">More</kbd> button
 
-				Done! ✨
+						<!--  -->
+					{:else if selectedTab === 1}
+						Click the <kbd class="kbd kbd-sm">Share</kbd> button
 
-				<br />
+						<!--  -->
+					{:else}
+						-
+					{/if}
+				</li>
 
-				{#if selectedTab === 0}
-					You can open the App from your Home Screen
+				<li>
+					<h3 class="text-lg font-semibold">Step 3</h3>
 
-					<!--  -->
-				{:else if selectedTab === 1}
-					You can open the App from your Home Screen
+					{#if selectedTab === 0}
+						Click <kbd class="kbd kbd-sm">Install App</kbd>
 
-					<!--  -->
-				{:else}
-					You can open the App from your Desktop
-				{/if}
-			</li>
-		</ol>
-	</section>
-</main>
+						<div
+							class="block text-left tooltip tooltip-bottom tooltip-primary mt-2"
+							data-tip="Try again later! Or just continue, it will still work"
+						>
+							<button class="btn btn-ghost btn-xs no-animation pl-0"> Only see "Add to Home Screen"? </button>
+						</div>
+
+						<!--  -->
+					{:else if selectedTab === 1}
+						Click <kbd class="kbd kbd-sm">Add to Home Screen</kbd>
+
+						<!--  -->
+					{:else}
+						Click <kbd class="kbd kbd-sm">Install</kbd>
+					{/if}
+				</li>
+
+				<li>
+					<h3 class="text-lg font-semibold">Step 4</h3>
+
+					Done! ✨
+
+					<br />
+
+					{#if selectedTab === 0}
+						You can open the App from your Home Screen
+
+						<!--  -->
+					{:else if selectedTab === 1}
+						You can open the App from your Home Screen
+
+						<!--  -->
+					{:else}
+						You can open the App from your Desktop
+					{/if}
+				</li>
+			</ol>
+		</section>
+	</main>
+</div>
