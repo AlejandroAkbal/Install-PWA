a5efd51c7fb5ae37ea6af168df65925b45228f1b
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2023-01-14 11:02:31 +0100

    fix(og image): capture installpwa instead of the original website

--- src/routes/from/[domain]/+page.js
+++ src/routes/from/[domain]/+page.js
@@ -1,4 +1,5 @@
 import { error } from '@sveltejs/kit'
+import { PUBLIC_APP_URL } from '$env/static/public'
 
 /** @type {import('./$types').PageLoad} */
 export function load({ params }) {
@@ -6,11 +7,14 @@ export function load({ params }) {
 		throw error(404, 'Not found')
 	}
 
+	// Poor mans router
+	const currentUrl = encodeURIComponent(PUBLIC_APP_URL + '/from/' + params.domain)
+
 	return {
 		title: params.domain,
 		description: `Learn how to install the ${params.domain} app!`,
 
-		imageUrl: `https://screenshot.akbal.dev/v1/capture?width=1200&height=630&url=${params.domain}`,
+		imageUrl: `https://screenshot.akbal.dev/v1/capture?width=1200&height=630&delay=2&mime_type=jpeg&url=${currentUrl}`,
 
 		domain: params.domain
 	}
