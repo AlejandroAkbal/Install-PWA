8ff5900e7087f7184dd7dff3f59123b8b4ddc288
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2024-11-01 10:57:13 +0000

    ci: try to fix subpath

--- svelte.config.js
+++ svelte.config.js
@@ -7,9 +7,6 @@ const config = {
 		adapter: adapter({
 			fallback: '404.html'
 		}),
-		paths: {
-			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
 	},
 
 	preprocess: [vitePreprocess()]
