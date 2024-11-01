f8f00c4abbd167a8265282adcef9ad9f69817182
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2024-11-01 09:58:24 +0000

    feat: prepare for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,18 @@
-import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-static'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	},
 
 	preprocess: [vitePreprocess()]
-};
+}
 
-export default config;
+export default config
