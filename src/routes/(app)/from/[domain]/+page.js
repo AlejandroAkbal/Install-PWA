528a9a1d7afffdf3b7888da5b9d515064e00bc53
Alejandro Akbal <37181533+AlejandroAkbal@users.noreply.github.com>
2024-11-06 13:43:41 +0000

    fix: use correct ways to get current url

--- src/routes/(app)/from/[domain]/+page.js
+++ src/routes/(app)/from/[domain]/+page.js
@@ -1,20 +1,16 @@
 import { error } from '@sveltejs/kit'
-import { ORIGIN } from '$env/dynamic/private'
 
 /** @type {import('./$types').PageLoad} */
-export function load({ params }) {
+export function load({ url, params }) {
 	if (!params.domain) {
 		error(404, 'Not found')
 	}
-
-	// Poor mans router
-	const currentUrl = encodeURIComponent(ORIGIN + '/from/' + params.domain)
 
 	return {
 		title: `Install the ${params.domain} web app`,
 		description: `Learn how to install the ${params.domain} web app in your device! It does not require any permissions and does not take up any space on your device.`,
 
-		imageUrl: `https://screenshot.akbal.dev/v1/capture?width=1200&height=630&delay=2&mime_type=jpeg&url=${currentUrl}`,
+		imageUrl: `https://screenshot.akbal.dev/v1/capture?width=1200&height=630&delay=2&mime_type=jpeg&url=${url.href}`,
 
 		domain: params.domain
 	}
